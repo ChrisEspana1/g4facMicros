cb2d3bbc04c949641acbdbef2e1be9a4add362bd
Christony560 <espanachristian560@gmail.com>
2023-10-09 15:22:15 -0600

    Creacion - Logica para nuevo usuario con microserv

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -23,9 +23,62 @@ class Login extends CI_Controller
     }
 
     public function newregistro() //Generamos registros para la tabla Usuario y Cliente
-    {  
-        
+    {
+        $data = array(
+            'usuario_nombre' => $this->input->post('nombre'),
+            'usuario_apellido' => $this->input->post('apellido'),
+            'usuario_email' => $this->input->post('email'),
+            'usuario_password' => $this->input->post('password'),
+            'usuario_estado' => $this->input->post('estado'),
+            'usuario_rol' => $this->input->post('rol')
+        );
+
+
+        $options = array(
+            'http' => array(
+                'method' => 'POST',
+                'header' => 'Content-type: application/json',
+                'content' => json_encode($data)
+            )
+        );
+
+
+        $context = stream_context_create($options);
+        $result = file_get_contents('http://localhost:8081/api/postusuario', false, $context);
+
+        if ($result === FALSE) {
+            echo "Error al enviar los datos a la API";
+        } else {
+            echo "Los datos se han enviado correctamente a la API";
+        }
     }
+
+    public function newcliente()
+    {
+        $clientedta = array(
+            'Cliente_nombre' => $this->input->post('nombre'),
+            'Cliente_apellido' => $this->input->post('apellido'),
+        );
+
+        $option2 = array(
+            'http' => array(
+                'method' => 'POST',
+                'header' => 'Content-type: application/json',
+                'content' => json_encode($clientedta)
+            )
+        );
+
+        $context = stream_context_create($option2);
+        $result = file_get_contents('http://localhost:8081/api/postusuario', false, $context);
+
+        if ($result === FALSE) {
+            echo "Error al enviar los datos a la API";
+        } else {
+            echo "Los datos se han enviado correctamente a la API";
+        }
+    }
+
+
 
     public function logout()
     {
